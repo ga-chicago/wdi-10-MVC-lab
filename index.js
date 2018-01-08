d98bd84e3b1bc5398721c8a54c103e4332b87078
Michael Weber <webermn15@gmail.com>
2018-01-08 17:01:31 -0600

    added files from daily lesson

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+
+//we must now require our fruit model file since we moved it to another file
+const fruit = require('./models/fruits.js')
+
+
+
+
+
+
+app.get('/', (req,res) => {
+	res.send('connection established')
+})
+
+
+app.get('/fruit', (req,res) => {
+	res.send(fruit);
+})
+
+
+app.get('/fruit/:id', (req,res) => {
+
+	//WE'RE GONNA MAKE THIS ALSO SEND AN OBJECT WITH OUT DATA
+	//THE SECOND PARAMETER MUST BE AN OBJECT
+	//we're going to use that object to include data in our template
+	//res.render() 'renders' a template / view, whereas res.send just sends text
+	res.render('show.ejs', {
+		fruit: fruit[req.params.id]
+	});
+
+
+
+
+
+
+	// const fruitId = req.params.id;
+	// // console.log(fruitId); 
+	// let responseString = 'fruit #' + fruitId + ' is a ' + fruit[fruitId].name + ' and is ';
+	// if (!fruit[fruitId].ripe) {
+	// 	responseString += 'not ';
+	// }
+	// responseString += 'ready to eat';
+	// res.send(''+responseString);
+})
+
+
+
+
+
+
+
+app.listen(3000, () => {
+	console.log('listening on port 3000');
+})
---
+++ models/fruits.js
@@ -0,0 +1,34 @@
+const fruits = [{
+	name: 'apple',
+	color: 'red',
+	ripe: true
+	}, 
+	{
+	name: 'papaya',
+	color: 'yellow',
+	ripe: false
+	}, 
+	{
+	name: 'mango',
+	color: 'orange',
+	ripe: true
+	}, 
+	{
+	name: 'pineapple',
+	color: 'spiky',
+	ripe: false
+	}, 
+	{
+	name: 'dragonfruit',
+	color: 'green',
+	ripe: true
+}];
+
+
+
+
+module.exports = fruits;
+
+
+
+
