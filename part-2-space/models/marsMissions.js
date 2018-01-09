4442b623b292878a5e8e87bae4581385ba75465b
Michael Weber <webermn15@gmail.com>
2018-01-09 09:00:24 -0600

    added img links to marsMissions object

--- part-2-space/models/marsMissions.js
+++ part-2-space/models/marsMissions.js
@@ -4,35 +4,35 @@ const marsMissions = [
     launchDate: "26 Nov 2011",
     operator: "NASA",
     missionType: "Rover",
-    img: ""
+    img: "https://img.purch.com/w/660/aHR0cDovL3d3dy5zcGFjZS5jb20vaW1hZ2VzL2kvMDAwLzA1Ni83NDUvb3JpZ2luYWwvbWFycy1yb3Zlci1jdXJpb3NpdHktZHJpbGwtaG9sZS5qcGc="
   },
   {
     name: "Opportunity",
     launchDate: "8 Jul 2003",
     operator: "NASA",
     missionType: "Rover",
-    img: ""
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/KSC-03PD-0786.jpg/1200px-KSC-03PD-0786.jpg"
   },
   {
     name: "Spirit",
     launchDate: "10 Jun 2003",
     operator: "NASA",
     missionType: "Rover",
-    img: ""
+    img: "https://nssdc.gsfc.nasa.gov/planetary/image/mars2003_rover.jpg"
   },
   {
     name: "Sojourner",
     launchDate: "4 Dec 1996",
     operator: "NASA",
     missionType: "Rover",
-    img: ""
+    img: "https://images.fineartamerica.com/images-medium-large/model-of-the-mars-pathfinder-rover-sojourner-nasa.jpg"
   },
   {
     name: "Rosetta",
     launchDate: "2 Mar 2004",
     operator: "ESA",
     missionType: "Gravity Assist",
-    img: ""
+    img: "https://img.purch.com/w/660/aHR0cDovL3d3dy5zcGFjZS5jb20vaW1hZ2VzL2kvMDAwLzAzNS8yMzkvb3JpZ2luYWwvUm9zZXR0YS1QaGlsYWUtQXJ0aXN0LUltcHJlc3Npb24uanBn"
   }
 ];
 
