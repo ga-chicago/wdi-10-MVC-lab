fec797de1ba01c12f43f58c3ea9f51592cf93c52
Anthony <anthonyjlower@gmail.com>
2018-01-08 23:58:22 -0600

    Completed fruits section

---
+++ REST/models/fruits.js
@@ -0,0 +1,29 @@
+const fruits = [{
+		name: "apple",
+		readyToEat: true,
+		color: "red"	
+	},
+	{
+		name: "banana",
+		readyToEat: true,
+		color: "yellow"	
+	},
+	{
+		name: "pear",
+		readyToEat: true,
+		color: "green"	
+	},
+	{
+		name: "guava",
+		readyToEat: true,
+		color: "green"	
+	},
+	{
+		name: "pineapple",
+		readyToEat: false,
+		color: "brown"
+	}
+];
+
+
+module.exports = fruits;
---
+++ REST/server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+// we must now require our fruit model since data has been moved out of this file
+
+const fruits = require('./models/fruits.js')
+
+
+// This represents what will later be a database
+
+
+app.get("/", (req, res) => {
+	res.send("Hello world");
+})
+
+
+app.get("/fruits", (req, res) => {
+	res.render("index.ejs")
+	// console.log("In index")
+	// res.render("show.ejs")
+
+})
+
+
+
+
+
+app.get("/fruits/:index", (req, res) => {
+	
+	// We are going to make this send an object with our data
+	// The 2nd parameter must be an object
+	// We are going to use that object to show data in our template
+	// res.render() renders an template/view where res.send() only just sends text
+
+	res.render("show.ejs", {
+		fruit: fruits[req.params.index]
+	});
+
+
+
+	// const fruitIndex = req.params.index;
+	
+	// let responseString = "Fruit # "+ fruitIndex+ " is "+ fruits[fruitIndex].name+ " and is ";
+	// if(!fruits[fruitIndex].readyToEat) responseString = responseString + "not ";
+	// responseString += "ready to eat"
+
+	// res.send(responseString);
+})
+
+
+
+
+app.listen(3000, () => {
+	console.log("Listening on port 3000");
+})
