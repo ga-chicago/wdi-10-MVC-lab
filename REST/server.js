3766dcf41a1c6c425efa987e147c1b39d95b91c7
Kait Mikitin <kait.mikitin@gmail.com>
2018-01-09 09:07:04 -0600

    Commit 3 - fixed my fruits

--- REST/server.js
+++ REST/server.js
@@ -13,43 +13,19 @@ app.get('/', (req, res) => {
 	res.send('Hello world');
 });
 
-app.get('/fruits/apple', (req, res) => {
-	res.render('apple.ejs', {
-		apple: fruits[0]
-	})
-})
-
-app.get('/fruits/banana', (req, res) => {
-	res.render('banana.ejs', {
-		banana: fruits[1]
-	})
-})
-
-app.get('/fruits/pear', (req, res) => {
-	res.render('pear.ejs', {
-		pear: fruits[2]
-	})
-})
-
-app.get('/fruits/guava', (req, res) => {
-	res.render('guava.ejs', {
-		guava: fruits[3]
-	})
-})
-
-app.get('/fruits/pineapple', (req, res) => {
-	res.render('pineapple.ejs', {
-		pineapple: fruits[4]
-	})
-})
-
 app.get('/fruits', (req, res) => {
+	res.render('index.ejs', {
+		fruit: fruits
+	})
+})
+
+app.get('/fruits/:id', (req, res) => {
 	// send an OBJECT with our data
 	// the second parameter must be an OBJECT
 	// use that object to include data in our template
 	// res.render renders a template/view where res.send() just sends text
 	res.render('show.ejs', {
-		fruit: fruits
+		fruit: fruits[req.params.id]
 	});
 });
 
